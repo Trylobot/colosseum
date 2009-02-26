49de0ccb62f5b680447e30e4d609c3c648ae152a
tylerbot@gmail.com <tylerbot@gmail.com@69fcb317-3e8f-4638-8c51-8e7de7714816>
2009-02-26 03:15:42 +0000

    git-svn-id: https://tylerbot.unfuddle.com/svn/tylerbot_colosseum@571 69fcb317-3e8f-4638-8c51-8e7de7714816

--- zip.bat
+++ zip.bat
@@ -1,2 +1,2 @@
-7z a -tzip colosseum.zip Colosseum.exe art/*.png data/*.colosseum_data fonts/*.ttf levels/*.colosseum_level sound/*.ogg
+7z a -tzip colosseum.zip Colosseum.exe art/*.png data/*.colosseum_data fonts/*.ttf levels/*.colosseum_level sound/*.ogg user/*.colosseum_*
 rem pause
