aacfaccd3435e52550a3d374c3b7e3792d0cfa85
Tylerbot <Tylerbot@69fcb317-3e8f-4638-8c51-8e7de7714816>
2009-09-09 01:35:46 +0000

    git-svn-id: https://tylerbot.unfuddle.com/svn/tylerbot_colosseum@659 69fcb317-3e8f-4638-8c51-8e7de7714816

--- create_zip_distro.bat
+++ create_zip_distro.bat
@@ -1,1 +1,25 @@
-7z a -tzip colosseum.zip Colosseum.exe art/*.png data/*.colosseum_data fonts/*.ttf levels/*.colosseum_level sound/*.ogg user/*.colosseum_*
+rem   Colosseum distribution .zip file maker
+
+mkdir __distro_7z_temp__
+mkdir __distro_7z_temp__\art
+mkdir __distro_7z_temp__\data
+mkdir __distro_7z_temp__\fonts
+mkdir __distro_7z_temp__\levels
+mkdir __distro_7z_temp__\sound
+mkdir __distro_7z_temp__\user
+
+copy Colosseum.exe              __distro_7z_temp__
+copy art\*.png                  __distro_7z_temp__\art
+copy data\*.colosseum_data      __distro_7z_temp__\data
+copy fonts\*.ttf                __distro_7z_temp__\fonts
+copy levels\*.colosseum_level   __distro_7z_temp__\levels
+copy sound\*.ogg                __distro_7z_temp__\sound
+copy user_release\*.colosseum_* __distro_7z_temp__\user
+
+cd __distro_7z_temp__
+
+7z a -r -tzip ../distros/colosseum__win.zip *.*
+
+cd ..
+rmdir /S /Q __distro_7z_temp__
+
