206bf89b6d6da8ef6a9b0335c8ee2f947bb0f172
Tylerbot <Tylerbot@69fcb317-3e8f-4638-8c51-8e7de7714816>
2009-09-30 19:46:00 +0000

    fixed bug with distro thingie
    git-svn-id: https://tylerbot.unfuddle.com/svn/tylerbot_colosseum@736 69fcb317-3e8f-4638-8c51-8e7de7714816

--- create_zip_distro.bat
+++ create_zip_distro.bat
@@ -9,9 +9,7 @@ mkdir __distro_7z_temp__\levels
 mkdir __distro_7z_temp__\sound
 mkdir __distro_7z_temp__\user
 
-mkdir distros
-
-xcopy Colosseum.exe              __distro_7z_temp__        %FLAGS%
+copy  Colosseum.exe              __distro_7z_temp__        
 xcopy art\*.png                  __distro_7z_temp__\art    %FLAGS%
 xcopy data\*.colosseum_data      __distro_7z_temp__\data   %FLAGS%
 xcopy fonts\*.ttf                __distro_7z_temp__\fonts  %FLAGS%
@@ -19,6 +17,7 @@ xcopy levels\*.colosseum_level   __distro_7z_temp__\levels %FLAGS%
 xcopy sound\*.ogg                __distro_7z_temp__\sound  %FLAGS%
 xcopy user_release\*.colosseum_* __distro_7z_temp__\user   %FLAGS%
 
+mkdir distros
 cd __distro_7z_temp__
 
 7z a -r -tzip ../distros/colosseum__win.zip *.*
